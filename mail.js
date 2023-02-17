57b89f83875d2b785a85876af0d54161536f3ffb
Dmitriy o_HET <dimati9@mail.ru>
2023-02-18 04:53:15 +0500

    Главная + опрос

--- mail.js
+++ mail.js
@@ -2,7 +2,15 @@
 $(function() {
     $(".box").on("click", e => {
         console.log($(e.target));
+        if($(e.target)[0].nodeName == "INPUT") {
+            return;
+        }
         $(e.target).parents(".box").find(".full").toggleClass("show");
         // $(this).parent(".box").find(".full").toggleClass("show");
-    })
+    });
+
+    $("#link").click(e => {
+        localStorage.setItem("info", "1");
+        location.href = "./index.html";
+    });
 });
